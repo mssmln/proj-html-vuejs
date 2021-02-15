1c58498dd3cefdc15afea8b82b31a3d279a97b8f
Massimiliano <m.croci.ly@gmail.com>
2021-02-15 16:53:24 +0100

    photos area

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@ var app = new Vue ({
   el: '#app',
   data: {
     header: ['sign in','about us','contact us','buy now'],
+    mainMenu: ['home','elements','features','pages','portfolio','blog','shop'],
+    photos: 'blog-4',
+    imgTag: ['photography','gadgets','travel','lifestyle','travel','recipes'],
+    imgHeadline: ['How To Take Better Concert Pictures in 30 Seconds','Gadgets That Make Your Smartphone Even Smarter','20 Top-Rated Tourist Attractions in Manhattan','The Best Way to Ride a Motorcycle','5 Fun Things to Do at the Beach','Amazingly Fresh Fruit And Herb Drinks For Summer'],
     footer: ['FAQs','Sitemap','Contact us']
   },
 });

df7e1c53fa0d7e1a308beb67d261d3e1b5d3986d
Massimiliano <m.croci.ly@gmail.com>
2021-02-16 11:32:51 +0100

    articles

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ var app = new Vue ({
     header: ['sign in','about us','contact us','buy now'],
     mainMenu: ['home','elements','features','pages','portfolio','blog','shop'],
     photos: 'blog-4',
-    imgTag: ['photography','gadgets','travel','lifestyle','travel','recipes'],
+    slider: ['photography','gadgets','travel','lifestyle','travel','recipes','fashion'],
     imgHeadline: ['How To Take Better Concert Pictures in 30 Seconds','Gadgets That Make Your Smartphone Even Smarter','20 Top-Rated Tourist Attractions in Manhattan','The Best Way to Ride a Motorcycle','5 Fun Things to Do at the Beach','Amazingly Fresh Fruit And Herb Drinks For Summer'],
     footer: ['FAQs','Sitemap','Contact us']
   },

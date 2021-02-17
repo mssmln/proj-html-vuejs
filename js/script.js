243df2c875b04b24e9da6111bc317e627c47bdb4
Massimiliano <m.croci.ly@gmail.com>
2021-02-17 14:05:55 +0100

    footer

--- js/script.js
+++ js/script.js
@@ -4,9 +4,21 @@ var app = new Vue ({
     header: ['sign in','about us','contact us','buy now'],
     mainMenu: ['home','elements','features','pages','portfolio','blog','shop'],
     photos: 'blog-4',
-    slider: ['photography','gadgets','travel','lifestyle','travel','recipes','fashion'],
+    slider: ['photography','gadgets','lifestyle','travel','recipes','fashion'],
     imgHeadline: ['How To Take Better Concert Pictures in 30 Seconds','Gadgets That Make Your Smartphone Even Smarter','20 Top-Rated Tourist Attractions in Manhattan','The Best Way to Ride a Motorcycle','5 Fun Things to Do at the Beach','Amazingly Fresh Fruit And Herb Drinks For Summer'],
     tags: ['design','brands','video','business','travel'],
-    footer: ['FAQs','Sitemap','Contact us']
+    categories: ['business','architecture','reviews','sports','videos','technology','design'],
+    footer: ['FAQs','Sitemap','Contact us'],
+    arrConcatenato: ''
   },
+  mounted(){
+    this.arrConcatenato = this.joinArray(this.slider,this.categories);
+    console.log(this.arrConcatenato);
+  },
+  methods: {
+    joinArray(arr1,arr2){
+      let newArr = arr1.concat(arr2);
+      return newArr;
+    },
+  }
 });
